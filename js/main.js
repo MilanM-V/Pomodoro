ff50844fe016fd905b9a2f917f30f63b770d322a
Milan <2507milan@gmail.com>
2024-12-23 19:10:22 +0100

    start parametre musique

--- js/main.js
+++ js/main.js
@@ -145,8 +145,8 @@ window.addEventListener("wheel", function(e) {
 }, { passive: false });
 
 const playlist = [
-    './musique/3-HOUR Yokohama at Sunset.mp3',
-    './musique/2-HOUR Shibuya.mp3'
+    "./musique/arcane/Arcane (League of Legends) - I Can't Hear It Now - Solo Piano [+ Sheet Music] [8YFFXHfKP1o].mp3",
+    "./musique/arcane/Stromae, Pomme - Ma Meilleure Ennemie (from Arcane Season 2) - Piano Cover ⧸ Version [0Z7jXlBV_nU].mp3"
     
 ];
 let currentTrackIndex = 0;
@@ -174,6 +174,21 @@ function playNext() {
     cover()
     title()
 }
+
+function playPrevious(){
+    if (currentTrackIndex==0){
+        currentTrackIndex=playlist.length-1
+    }else{
+        currentTrackIndex=currentTrackIndex-1
+    }
+    const audioSource = document.getElementById('audio-source');
+    audioSource.src = playlist[currentTrackIndex];
+    audioPlayer.load();
+    audioPlayer.play();
+    cover()
+    title()
+}
+
 
 function cover(){
     fetch(playlist[currentTrackIndex])
@@ -222,7 +237,28 @@ function duree(){
                 var heure = Math.floor(temps_musique / 3600);
                 var minutes = Math.floor((temps_musique % 3600) / 60);
                 var seconds = Math.floor(temps_musique % 60);
-                document.getElementById("duree").innerHTML ="-"+heure+':'+minutes + ":"+'0' + seconds;
+                if (heure>0){
+                    if(minutes<10 && seconds>9){
+                        document.getElementById("duree").innerHTML ="-"+heure+':'+'0'+minutes + ":" + seconds;
+                    }else if(minutes<10 && seconds<10){
+                        document.getElementById("duree").innerHTML ="-"+heure+':'+'0'+minutes + ":" + "0"+seconds;
+                    }else if(minutes>9 && seconds<10){
+                        document.getElementById("duree").innerHTML ="-"+heure+':'+minutes + ":" + "0"+seconds;
+                    }else{
+                        document.getElementById("duree").innerHTML ="-"+heure+':'+minutes + ":" + seconds;
+                    }
+                }else{
+                    if(minutes<10 && seconds>9){
+                        document.getElementById("duree").innerHTML ="-"+'0'+minutes + ":" + seconds;
+                    }else if(minutes<10 && seconds<10){
+                        document.getElementById("duree").innerHTML ="-"+'0'+minutes + ":" + "0"+seconds;
+                    }else if(minutes>9 && seconds<10){
+                        document.getElementById("duree").innerHTML ="-"+minutes + ":" + "0"+seconds;
+                    }else{
+                        document.getElementById("duree").innerHTML ="-"+minutes + ":" + seconds;
+                    }
+                }
+                
             }
         }    
     audioPlayer.ontimeupdate = function(){
@@ -234,17 +270,113 @@ function duree(){
                 var heure = Math.floor(temps_musique / 3600);
                 var minutes = Math.floor((temps_musique % 3600) / 60);
                 var seconds = Math.floor(temps_musique % 60);
-                if(minutes<10 && seconds>9){
-                    document.getElementById("duree").innerHTML ="-"+heure+':'+'0'+minutes + ":" + seconds;
-                }else if(minutes<10 && seconds<10){
-                    document.getElementById("duree").innerHTML ="-"+heure+':'+'0'+minutes + ":" + "0"+seconds;
-                }else if(minutes>9 && seconds<10){
-                    document.getElementById("duree").innerHTML ="-"+heure+':'+minutes + ":" + "0"+seconds;
+                if (heure>0){
+                    if(minutes<10 && seconds>9){
+                        document.getElementById("duree").innerHTML ="-"+heure+':'+'0'+minutes + ":" + seconds;
+                    }else if(minutes<10 && seconds<10){
+                        document.getElementById("duree").innerHTML ="-"+heure+':'+'0'+minutes + ":" + "0"+seconds;
+                    }else if(minutes>9 && seconds<10){
+                        document.getElementById("duree").innerHTML ="-"+heure+':'+minutes + ":" + "0"+seconds;
+                    }else{
+                        document.getElementById("duree").innerHTML ="-"+heure+':'+minutes + ":" + seconds;
+                    }
                 }else{
-                    document.getElementById("duree").innerHTML ="-"+heure+':'+minutes + ":" + seconds;
+                    if(minutes<10 && seconds>9){
+                        document.getElementById("duree").innerHTML ="-"+'0'+minutes + ":" + seconds;
+                    }else if(minutes<10 && seconds<10){
+                        document.getElementById("duree").innerHTML ="-"+'0'+minutes + ":" + "0"+seconds;
+                    }else if(minutes>9 && seconds<10){
+                        document.getElementById("duree").innerHTML ="-"+minutes + ":" + "0"+seconds;
+                    }else{
+                        document.getElementById("duree").innerHTML ="-"+minutes + ":" + seconds;
+                    }
                 }
+                
             }
         }
     }
 }
 duree()
+
+
+// Obtenir les éléments
+const modal = document.getElementById('modal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeModalBtn = document.getElementById('closeModalBtn');
+const modal_content=document.getElementById('modal-content');
+
+openModalBtn.onclick = function() {
+    modal.style.display = "block"; 
+    modal_content.style.display="block"
+    setTimeout(function() {
+        modal_content.style.top = '-20%';
+        modal.style.opacity = 1;
+    }, 10);
+
+}
+
+closeModalBtn.onclick = function() {
+    modal.style.opacity = 0; 
+    modal_content.style.top = '-50%';
+    setTimeout(function() {
+        modal.style.display = "none";
+        modal_content.style.display = "none";
+    }, 500); 
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.opacity = 0; 
+        modal_content.style.top = '-50%';
+        setTimeout(function() {
+            modal.style.display = "none"; 
+            modal_content.style.display = "none";
+        }, 500);
+    }
+}
+
+var fond_div=document.getElementById('fond-parametre')
+var musique_div=document.getElementById('musique-parametre')
+
+function show_music(){
+    musique_div.style.opacity=1
+    fond_div.style.opacity=0
+}
+
+function show_fond(){
+    
+    fond_div.style.opacity=1
+    musique_div.style.opacity=0
+}
+
+var In=true
+
+// Récupérer les données de localStorage au chargement
+var playlist_select = JSON.parse(localStorage.getItem('playlist_select')) || [];
+
+// Appliquer l'état initial des cases à cocher
+document.querySelectorAll('#playlist input').forEach(input => {
+    if (playlist_select.includes(input.name)) {
+        input.checked = true;
+    }
+});
+
+document.querySelectorAll('#playlist input').forEach(input =>{
+    input.addEventListener('change',()=>{
+        In=true
+        for (let i=0;i<playlist_select.length;i++){
+            console.log(playlist_select[i]+":"+input.name)
+            if (playlist_select[i]==input.name)
+                In=false
+        }
+        if (input.checked){
+            if (In==true){
+                playlist_select.push(input.name)
+            }
+        }else{
+            playlist_select=playlist_select.filter(item => item !==input.name)
+        }
+        localStorage.setItem('playlist_select', JSON.stringify(playlist_select));
+        console.log(playlist_select)
+    })
+})
